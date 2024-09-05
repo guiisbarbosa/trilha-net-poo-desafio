061e2e4dbe33371fd82e3633c498fe0c1c9158f4
guiisbarbosa <gui.silva93@hotmail.com>
2024-09-05 19:39:49 -0300

    Exercício pronto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celular1 = new Nokia("991122", "Lumia10", "#123", 128);
+Console.WriteLine("Dados do celular1: " + celular1.Numero);
+celular1.InstalarAplicativo("Whatsapp");
+celular1.Ligar();
+celular1.ReceberLigacao();
+
+Iphone celular2 = new Iphone("981122", "iPhone 15", "#456", 256);
+Console.WriteLine("Dados do celular2: " + celular2.Numero);
+celular2.InstalarAplicativo("Facebook");
+celular2.Ligar();
+celular2.ReceberLigacao();
